420d8274c909f670a8ceed261d15033f2790cc81
MesaJonathan <72950525+MesaJonathan@users.noreply.github.com>
2022-06-22 15:58:35 -0400

    app shows cat

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from tkinter import *
+from PIL import ImageTk, Image
 from cat_get import get_cat
 
 root = Tk()
+root.iconbitmap('cat_icon.ico')
+root.title('MEEEEEEOOOOOWWW')
+
+img = ImageTk.PhotoImage(Image.open("cat.png"))
+myLabel = Label(root, image = img)
+myLabel.pack(side = "bottom", fill = "both", expand = "yes")
+
+root.mainloop()
